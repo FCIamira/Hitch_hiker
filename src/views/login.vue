032cc337254cda1bbdefbf71b1c0eb33ef5d1ae8
Amira Hashem <hashemamira96@gmail.com>
2023-09-12 08:17:10 -0400

    edit login &&sinup

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,105 +1,103 @@
 <template>
-  <div class="backg grid grid-cols-2 grid-flow-col gap-4">
-    <div><img src="../assets/login-page.png" /></div>
-    <!-- <div class="bg-black h-screen w-full"> -->
-    <form
-      @submit.prevent="login"
-      class="bg-white mt-24 p-2 w-3/4 h-4/6 mar order-solid border-2 rounded-lg drop-shadow-2xl"
-    >
-      <div id="label1" class="pt-8 w-full text-center pb-2">
-        <label for="username" class="block text-sm font-bold text-black">
-        </label>
-        <input
-          type="text"
-          class="p-4 w-10/12 border-2 border-slate-400 placeholder-black"
-          v-model="username"
-          placeholder="Enter your name"
-        />
-        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-          Please provide a valid .
-        </p>
-        <div class="w-12"></div>
-      </div>
+<div
+class="backg grid items-center md:grid-cols-2 grid-cols-1 grid-flow-col gap-4"
+>
+<div class="md:block hidden">
+<img src="../assets/login-page.png" />
+</div>
 
-      <div id="label2" class="pt-8 w-full text-center pb-2">
-        <!-- <label for="password" class="block text-sm font-bold text-black">password</label> -->
-        <input
-          v-model="password"
-          placeholder="Enter your password"
-          type="password" min="8"
-          class="w-10/12 border-2 border-slate-400 placeholder-black"
-        />
-        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-          Please provide a valid password .
-        </p>
-        <div class="labrl3 p-4 ml-11 flex flex-row-1">
+<!-- <div class="bg-black h-screen w-full"> -->
+<Form
+:validation-schema="schema" @submit="onSubmit"
+@submit.prevent="login"
+class="bg-white   p-2 w-3/4 h-4/4 md:m-0 mx-auto order-solid border-2 rounded-lg drop-shadow-2xl"
+>
+<div id="label3" class="pt-5 w-full text-center pb-2">
+
+<p class="text-3xl flex justify-center text-teal-500 font-bold">
+LOGIN
+</p>
+<label
+for="username"
+class="block text-sm font-bold text-black"
+></label>
+<Field
+type="text"
+name="firstname"
+class="p-4 m-4 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="firstname"
+placeholder="First Name"
+/>
+<ErrorMessage name="firstname" class="text-red-600 block" >
+</ErrorMessage>
+<Field
+type="password"
+name="password"
+class="p-4 m-2 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="password"
+placeholder="Enter your password"
+/>
+<ErrorMessage name="password"
+ class="text-red-600 block">
+</ErrorMessage>
+</div>
+<div class="labrl3 p-4 ml-11 flex flex-row-1">
             <label for="checkbox" class="mr-24  ">
               <input id="checkbox" type="checkbox" class="w-4 h-4 mr-3" />Remember me</label
             >
             <a href="#" style="color: blue"> ForgetPassword?</a>
         </div>
-        <div class="w-full text-center pb-2">
-          <button
-            @click="login"
-            class="mt-5 p-4 w-10/12 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-          >
-            Login
-          </button>
-        </div>
-      </div>
-      
-      <div class="registerlink p-4 ml-44">
+<div class="w-full text-center pb-2">
+
+<button
+@click="login"
+class="mt-5 p-4 w-10/12 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+>
+LOGIN
+</button>
+</div>
+<div class="registerlink p-4 mx-5 ml-12">
+<ErrorMessage>
+Alrady have an account ?!
+<a href="./login" style="color: blue"> Login?</a>
+</ErrorMessage>
+</div>
+<div class="registerlink p-4 ml-44">
         <p>
           Do not Have an account?!
           <a href="./signup" style="color: blue"> Register?</a>
         </p>
       </div>
-
-    </form>
-  </div>
-  
-  <p>{{ loginError }}</p>
+</Form>
+</div>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
+// import { useRouter } from "vue-router";
 import { ref } from "vue";
+import { Form, Field ,ErrorMessage} from 'vee-validate';
+import * as yup from 'yup';
 
-let username = ref("");
-let password = ref("");
-let errormsg = ref("errrrrror");
+const schema = yup.object({
+firstname:yup.string().required(),
 
-// let UsernameValid = ref(true);
-// let PasswordValid = ref(true);
-let loginError = ref("");
+password: yup.string().required().min(8),
+});
 
-let router = useRouter();
+let onSubmit=ref(()=> {
 
-const login = () => {
-  if (
-    username.value  &&
-    password.value 
-  ) {
-    router.push({ name: "home" });
-  }
-    else {
-    alert(errormsg.value)
-  }
-};
+})
 
-// const goToMain = () => {
-//   router.push({ name: "mainPage" });
-// };
 </script>
 <style>
-input {
-  width: 350px;
-  height: 50px;
-  padding: 20px;
+Field {
+width: 350px;
+height: 50px;
+padding: 20px;
 }
 .w-12 {
-  position: absolute;
-  right: 29px;
-  margin-top: -81px;
+position: absolute;
+right: 29px;
+margin-top: -81px;
 }
 </style>
--- src/views/signup.vue
+++ src/views/signup.vue
@@ -1,143 +1,144 @@
 <template>
-  <div
-    class="backg grid items-center md:grid-cols-2 grid-cols-1 grid-flow-col gap-4"
-  >
-    <div class="md:block hidden">
-      <img src="../assets/signup-img.png" />
-    </div>
+<div
+class="backg grid items-center md:grid-cols-2 grid-cols-1 grid-flow-col gap-4"
+>
+<div class="md:block hidden">
+<img src="../assets/signup-img.png" />
+</div>
 
-    <!-- <div class="bg-black h-screen w-full"> -->
-    <form
-      @submit.prevent="login"
-      class="bg-white mt-24 p-2 w-3/4 h-4/4 md:m-0 mx-auto order-solid border-2 rounded-lg drop-shadow-2xl"
-    >
-      <p class="text-3xl flex justify-center text-teal-500 font-bold">
-        SIGN-UP
-      </p>
-      <div id="label1" class="pt-8 w-full text-center ">
-        <label
-          for="username"
-          class="block text-sm font-bold text-black"
-        ></label>
-        <input
-          type="text"
-          class="p-4 w-10/12 border-2 border-slate-400 placeholder-black"
-          v-model="firstname"
-          placeholder="First Name"
-        />
-        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-          Please provide a valid .
-        </p>
-        <input
-          type="text"
-          class="p-4 w-10/12 border-2 border-slate-400 placeholder-black"
-          v-model="lastname"
-          placeholder="Last Name"
-        />
-        
-        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-          Please provide a valid .
-        </p>
-      </div>
+<!-- <div class="bg-black h-screen w-full"> -->
+<Form
+:validation-schema="schema" @submit="onSubmit"
+@submit.prevent="login"
+class="bg-white   p-2 w-3/4 h-4/4 md:m-0 mx-auto order-solid border-2 rounded-lg drop-shadow-2xl"
+>
+<div id="label3" class="pt-5 w-full text-center pb-2">
 
-      <div id="label2" class=" w-full text-center pb-2">
-        <input
-          type="number"
-          class="p-4 w-10/12 border-2 border-slate-400 placeholder-black"
-          v-model="phoneNumber"
-          placeholder="Phone Number"
-        />
-      </div>
-
-      <div id="label3" class="pt-5 w-full text-center pb-2">
-        <!-- <label for="password" class="block text-sm font-bold text-black">password</label> -->
-        <input
-          v-model="email"
-          placeholder="Enter your E-mail"
-          type="email"
-          class="w-10/12 border-2 border-slate-400 placeholder-black"
-        />
-        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-          Please provide a valid email .
-        </p>
-        <input
-          v-model="password"
-          placeholder="Enter your password"
-          type="password"
-          class="w-10/12 border-2 border-slate-400 placeholder-black"
-        />
-        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-          Please provide a valid password .
-        </p>
-        <div class="w-full text-center pb-2">
-          <button
-            @click="login"
-            class="mt-5 p-4 w-10/12 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-          >
-            Sign-Up
-          </button>
-        </div>
-        <div class="registerlink p-4 mx-5 ml-12">
+<p class="text-3xl flex justify-center text-teal-500 font-bold">
+SIGN-UP
+</p>
+<label
+for="username"
+class="block text-sm font-bold text-black"
+></label>
+<Field
+type="text"
+name="firstname"
+class="p-4 m-4 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="firstname"
+placeholder="First Name"
+/>
+<ErrorMessage name="firstname" class="text-red-600 block" >
+</ErrorMessage>
+<Field
+type="text"
+name="lastname"
+class="p-4 m-2 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="lastname"
+placeholder="Last Name"
+/>
+<ErrorMessage  name="lastname"
+ class="text-red-600 block ">
+</ErrorMessage>
+<Field
+type="number"
+name="phoneNumber"
+class="p-4 m-2 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="phoneNumber"
+placeholder="enter phoneNumber"
+/>
+<ErrorMessage class="text-red-600 block" name="phoneNumber"
+>
+</ErrorMessage>
+<Field
+type="email"
+name="email"
+class="p-4 m-2 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="email"
+placeholder="enter Email "
+/>
+<ErrorMessage name="email"
+ class="text-red-600 block">
+</ErrorMessage>
+<Field
+type="password"
+name="password"
+class="p-4 m-2 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="password"
+placeholder="Enter your password"
+/>
+<ErrorMessage name="password"
+ class="text-red-600 block">
+</ErrorMessage>
+<Field
+type="password"
+name="confirm_password"
+class="p-4 m-2 w-10/12 border-2 border-slate-400 rounded-lg placeholder-slate-600"
+v-model="confirm_password"
+placeholder="Enter your confirm_password"
+/>
+<ErrorMessage name="confirm_password"
+ class="text-red-600 block">
+</ErrorMessage>
+</div>
+<div class="w-full text-center pb-2">
+<button
+@click="login"
+class="mt-5 p-4 w-10/12 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+>
+Sign-Up
+</button>
+</div>
+<div class="registerlink p-4 mx-5 ml-12">
+<ErrorMessage>
+Alrady have an account ?!
+<a href="./login" style="color: blue"> Login?</a>
+</ErrorMessage>
+</div>
+<!-- </div> -->
+<div class="registerlink p-4 mx-5 ml-12">
           <p>
             Alrady have an account ?!
             <a href="./login" style="color: blue"> Login?</a>
           </p>
         </div>
-      </div>
-    </form>
-  </div>
-  <p>{{ loginError }}</p>
+</Form>
+</div>
+<p>{{ loginError }}</p>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
+// import { useRouter } from "vue-router";
 import { ref } from "vue";
+import { Form, Field ,ErrorMessage} from 'vee-validate';
+import * as yup from 'yup';
 
-let firstname = ref("");
-let lastname = ref("");
-let email = ref("");
-let phoneNumber = ref("");
-let password = ref("");
-let errormsg = ref("errrrrror");
+const schema = yup.object({
+firstname:yup.string().required(),
+lastname:yup.string().required(),
+email: yup.string().email().required(),
+phoneNumber:yup.string().required(),
+password: yup.string().required().min(8),
+confirm_password:yup.string()
+    .required()
+    .min(8)
+    .oneOf([yup.ref('password'), null], 'Passwords must match'), // Ensure password and confirm_password match
+});
 
+let onSubmit=ref((values)=> {
+alert(JSON.stringify(values, null, 2));
+})
 
-
-// let UsernameValid = ref(true);
-// let PasswordValid = ref(true);
-let loginError = ref("");
-
-let router = useRouter();
-
-const login = () => {
-  if (
-    firstname.value  &&
-    lastname.value  &&
-    password.value &&
-     email.value &&
-    phoneNumber.value
-
-
-  ) {
-    router.push({ name: "home" });
-  }
-   else {
-    alert(errormsg.value)
-  }
-};
-
-// const goToMain = () => {
-//   router.push({ name: "mainPage" });
-// };
 </script>
 <style>
-input {
-  width: 350px;
-  height: 50px;
-  padding: 20px;
+Field {
+width: 350px;
+height: 50px;
+padding: 20px;
 }
 .w-12 {
-  position: absolute;
-  right: 29px;
-  margin-top: -81px;
+position: absolute;
+right: 29px;
+margin-top: -81px;
 }
 </style>
