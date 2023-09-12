dbadfad7a8c5e20acde1b0482f7e0ee5dee1cf3c
hesham abdelnasser <et4awyabdelnasser@gmail.com>
2023-09-12 21:46:38 +0300

    dashboard

--- src/views/AddShipment.vue
+++ src/views/AddShipment.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contanier h-auto w-full bg-center bg-gray-100">
     <div class="flex justify-around pt-10">
-      <p class="text-4xl text-teal-400 font-bold font-light">Add Shipment</p>
+      <p class="text-4xl text-gray-800 font-bold ">Add Shipment</p>
     </div>
     <div
       class="sub-contanier h-auto md:p-4 p-2 gap-4 w-full flex md:flex-row flex-col flex-wrap drop-shadow-2xl"
@@ -45,7 +45,7 @@
         class="text-lg w-[98%] h-14 p-4 placeholder:text-black border-2 border-slate-400 rounded-lg bg-gray-100"
       />
       <button
-        class="h-16 w-full rounded-lg bg-teal-400 hover:bg-teal-500 text-xl"
+        class="h-16 w-[98%] rounded-lg bg-gray-800 hover:bg-gray-500 text-3xl text-white"
       >
         Add Shipment
       </button>
--- src/views/AddTrips.vue
+++ src/views/AddTrips.vue
@@ -1,59 +1,50 @@
 <template>
-  <div
-    class="contanier flex justify-center items-center bg-cover h-screen w-full bg-center"
-  >
-    <div class="sub-contanier drop-shadow-2xl">
-      <div class="flex justify-around pt-10">
-        <p class="text-4xl text-white font-light">Add Trips</p>
-      </div>
-      <div class="flex justify-around pt-20">
-        <input
-          type="text"
-          placeholder="From (City,Country)"
-          class="w-2/5 p-6 text-l h-10 border-2 border-slate-400 rounded-lg bg-gray-100"
-        />
-        <input
-          type="text"
-          placeholder="To (City,Country)"
-          class="w-2/5 p-6 text-l h-10 border-2 border-slate-400 rounded-lg bg-gray-100"
-        />
-      </div>
-      <div class="flex justify-around pt-20">
-        <input
-          type="number"
-          placeholder="Available Weight"
-          class="w-2/5 p-6 text-l h-10 border-2 border-slate-400 rounded-lg bg-gray-100"
-        />
-        <input
-          type="date"
-          placeholder="Dates"
-          class="w-2/5 p-6 text-l h-10 border-2 border-slate-400 rounded-lg bg-gray-100"
-        />
-      </div>
-      <div class="flex justify-around pt-20">
-        <textarea
-          placeholder="Trip note"
-          class="w-11/12 h-40 p-4 text-xl rounded-lg bg-gray-200"
-        ></textarea>
-      </div>
-      <div class="flex justify-around pt-20">
-        <button
-          class="w-2/5 text-l h-16 rounded-lg bg-teal-400 hover:bg-teal-500 text-xl"
-        >
-          Add Trip
-        </button>
-      </div>
+  <div class="contanier h-auto w-full bg-center bg-gray-100">
+    <div class="flex justify-around pt-10 mx-auto text-4xl font-bold">
+      Add Trips
+    </div>
+    <div
+      class="sub-contanier h-auto md:p-4 p-2 gap-4 w-full flex md:flex-row flex-col flex-wrap drop-shadow-2xl"
+    >
+      <input
+        type="text"
+        placeholder="From (City,Country)"
+        class="text-lg md:w-[48%] w-full my-2 h-14 p-4 placeholder:text-black border-2 border-slate-400 rounded-lg bg-gray-100"
+      />
+      <input
+        type="text"
+        placeholder="To (City,Country)"
+        class="text-lg md:w-[48%] w-full my-2 h-14 p-4 placeholder:text-black border-2 border-slate-400 rounded-lg bg-gray-100"
+      />
+
+      <input
+        type="number"
+        placeholder="Item Weight"
+        class="text-lg md:w-[48%] focus:outline-0 w-full my-2 h-14 p-4 placeholder:text-black border-2 border-slate-400 rounded-lg bg-gray-100"
+      />
+      <input
+        type="date"
+        placeholder="Dates"
+        class="text-lg md:w-[48%] w-full my-2 h-14 p-4 placeholder:text-black border-2 border-slate-400 rounded-lg bg-gray-100"
+      />
+
+      <textarea
+        type="text"
+        placeholder="Trip note"
+        class="text-xl md:w-[96%] resize-none my-2 p-4 placeholder:text-black border-2 border-slate-400 rounded-lg bg-gray-100"
+      />
+
+      <button
+        class="h-16 w-[96%] rounded-lg bg-gray-800 hover:bg-gray-500 text-3xl text-white"
+      >
+        Add Trips
+      </button>
     </div>
   </div>
 </template>
 <script setup></script>
 <style scoped>
 .sub-contanier {
-  width: 50%;
-  height: 750px;
-  margin: auto;
-  border: solid 2px rgba(245, 234, 234, 0.061);
   border-radius: 50px;
-  background-color: rgba(0, 0, 0, 0.712);
 }
 </style>
