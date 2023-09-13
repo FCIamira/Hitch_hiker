620d400a5258053b0671ec0bd8033466c7bc5318
hesham abdelnasser <et4awyabdelnasser@gmail.com>
2023-09-13 15:06:18 +0300

    add button

--- src/views/shipment.vue
+++ src/views/shipment.vue
@@ -1,2 +1,6 @@
-<template>shipment</template>
-<script setup></script>
+<template>
+<button class="w-40 h-16 bg-blue-700 text-white rounded-xl flex justify-center pt-5  ml-auto  mt-20 mr-10 ">Add AddShipment </button>
+</template>
+<script setup>
+
+</script>
